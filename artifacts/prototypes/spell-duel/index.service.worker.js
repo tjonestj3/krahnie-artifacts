1d9305466404e7b7607a10caaad13ef7cf4ae8d2
Thomas Jones <thomasjones@pop-os.tail1c35d9.ts.net>
2026-05-30 07:15:06 -0500

    Update spell-duel web build from feaabd0

--- artifacts/prototypes/spell-duel/index.service.worker.js
+++ artifacts/prototypes/spell-duel/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780004762|2191612';
+const CACHE_VERSION = '1780143305|1941189';
 /** @type {string} */
 const CACHE_PREFIX = 'Spell Duel-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
